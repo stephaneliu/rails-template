77b6f30ff90be2c055fddc0002c9c83201fbe8fa
Stephane Liu <sliu@sjliu.com>
2018-10-02 15:41:15 -1000

    Remove envrc

--- template.rb
+++ template.rb
@@ -97,27 +97,6 @@ reek_config = <<-EOL
 EOL
 create_file ".reek", reek_config
 
-envrc = <<-EOL
-RED='\\033[0;31m'
-NC='\\033[0m'
-WHITE='\\033[1;37m'
-CYAN='\\033[0;36m'
-
-export ENABLE_LIVE_RELOAD=true
-export PROJECT_RUBY_VERSION=`cat ./.ruby-version`
-
-printf \"${WHITE}Setting ${PURPLE}APP_CODE${NC} to ${BLUE}$APP_CODE${NC}\\n\"
-source /usr/local/share/chruby/chruby.sh && chruby $PROJECT_RUBY_VERSION
-if [ $? != 0 ]; then
-  printf \"${WHITE}######## ${RED}Houston, we've got a problem...$PROJECT_RUBY_VERSION UNKNOWN ${WHITE}########${NC}\\n\"
-else
-  printf \"${WHITE}Ruby changed to ${CYAN}$PROJECT_RUBY_VERSION${NC}\\n\"
-fi
-
-PATH_add bin
-EOL
-create_file ".envrc", envrc
-
 # Setup shoulda matchers
 shoulda_matchers = <<-EOL
 Shoulda::Matchers.configure do |config|
